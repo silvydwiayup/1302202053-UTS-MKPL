e1a0c55130b88a4e4d1e65c98d9ceabaf194548b
silvydwiayup <100912347+silvydwiayup@users.noreply.github.com>
2023-04-14 20:16:25 +0700

    delete variable address
    variable tersebut dihapus karena variable tersebut tidak digunakan

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -11,7 +11,6 @@ public class Employee {
 	private String firstName;
 	private String lastName;
 	private String idNumber;
-	private String address;
 	
 	private int yearJoined;
 	private int monthJoined;
@@ -31,12 +30,11 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
-		this.address = address;
 		this.yearJoined = yearJoined;
 		this.monthJoined = monthJoined;
 		this.dayJoined = dayJoined;
